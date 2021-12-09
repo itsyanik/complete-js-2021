7646965875494143c0045bab3c2e3b5b46f1a43e
itsYanik <allan.yanik@gmail.com>
2021-12-09 12:25:58 -0300

    Creates guess number game base code

--- 07-DOM-Manipulation/script.js
+++ 07-DOM-Manipulation/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const secretNumber = Math.floor(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Game over!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Game over!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
