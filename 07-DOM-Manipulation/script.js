ff0ec7e161d0b65849b12963226901ab9c832773
itsYanik <allan.yanik@gmail.com>
2021-12-09 12:50:46 -0300

    Cleans code

--- 07-DOM-Manipulation/script.js
+++ 07-DOM-Manipulation/script.js
@@ -3,33 +3,52 @@
 const secretNumber = Math.floor(Math.random() * 20) + 1;
 let score = 20;
 
+const message = msg => {
+  let message = document.querySelector('.message');
+
+  return (message.textContent = msg);
+};
+
+const calcScore = pts => {
+  let score = document.querySelector('.score');
+
+  return (score.textContent = pts);
+};
+
+const win = () => {
+  let number = document.querySelector('.number');
+  number.style.width = '30rem';
+  number.textContent = secretNumber;
+  document.querySelector('body').style.backgroundColor = '#60b347';
+  message('Correct number!');
+
+  return null;
+};
+
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
 
   if (!guess) {
-    document.querySelector('.message').textContent = 'No number!';
+    message('No number!');
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct number!';
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '30rem';
-    document.querySelector('.number').textContent = secretNumber;
+    win();
   } else if (guess > secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too high!';
+      message('Too high!');
       score--;
-      document.querySelector('.score').textContent = score;
+      calcScore(score);
     } else {
-      document.querySelector('.message').textContent = 'Game over!';
-      document.querySelector('.score').textContent = 0;
+      message('Game over!');
+      calcScore(0);
     }
   } else if (guess < secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too low!';
+      message('Too low!');
       score--;
-      document.querySelector('.score').textContent = score;
+      calcScore(score);
     } else {
-      document.querySelector('.message').textContent = 'Game over!';
-      document.querySelector('.score').textContent = 0;
+      message('Game over!');
+      calcScore(0);
     }
   }
 });
