9b432a6c1bb79285c435cbb3b93024cff3300679
itsYanik <allan.yanik@gmail.com>
2022-01-04 11:37:44 -0300

    Answers challenge 2

--- 09-data-structures-operators/script.js
+++ 09-data-structures-operators/script.js
@@ -144,3 +144,68 @@ printGoals(...game.scored);
 // Challenge 1-7
 team1 > team2 && console.log('Team 1 is more likey to win.');
 team1 < team2 && console.log('Team 2 is more likey to win.');
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, 
+    along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console 
+    (We already studied how to calculate averages, 
+    you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+    Odd of victory Bayern Munich: 1.33
+    Odd of draw: 3.25
+    Odd of victory Borrussia Dortmund: 6.5
+    Get the team names directly from the game object, don't hardcode them 
+    (except for "draw"). 
+  HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains 
+  the names of the players who scored as properties, 
+  and the number of goals as the value. 
+  In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+
+// Challenge 2-1
+
+for (const [index, goal] of Object.entries(game.scored)) {
+  console.log(`Goal ${Number(index) + 1}: ${goal}`);
+}
+
+// Challenge 2-2
+let oddAvg = 0;
+const oddsValues = Object.values(game.odds);
+
+for (const odd of oddsValues) oddAvg += odd;
+
+console.log(`Average Odd: ${(oddAvg / oddsValues.length).toFixed(2)}`);
+
+// Challenge 2-3
+const oddEntries = Object.entries(game.odds);
+const oddKeys = Object.keys(game.odds);
+
+for (const [team, odd] of oddEntries) {
+  console.log(`Odd of ${team}: ${odd} `);
+  console.log('-----------');
+}
+
+// Challenge 2-BONUS
+let scorers = {};
+const { scored } = game;
+
+for (const score of scored) {
+  scorers[score] = scorers[score] ? scorers[score] + 1 : 1;
+}
+
+console.log(scorers);
