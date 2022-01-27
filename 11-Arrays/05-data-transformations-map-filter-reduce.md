f6fddefd848205c6778757b44fb93eff8fcddc9c
itsYanik <allan.yanik@gmail.com>
2022-01-27 17:51:45 -0300

    Challenge 3

--- 11-Arrays/05-data-transformations-map-filter-reduce.md
+++ 11-Arrays/05-data-transformations-map-filter-reduce.md
@@ -12,5 +12,5 @@ The filter method as the name suggests allows us to filter elements in the origi
 
 ## Reduce
 
-We use the reduce method to boil down all elements of the original array into one single value. One practical use case is to add all the elements in the array together (`arr.reduce((acc, curr) => acc + curr)`). As it loops over the array, it keeps accumulating the values in the accumulator variable until the end. `arr.reduce(accumulator, currentElement, arr)`.
+We use the reduce method to boil down all elements of the original array into one single value. One practical use case is to add all the elements in the array together (`arr.reduce((acc, curr) => acc + curr)`). As it loops over the array, it keeps accumulating the values in the accumulator variable until the end. `arr.reduce(accumulator, currentElement, index, arr)`.
 We say that this method reduces the array to one single value, this is why we call it reduce method. There is no array returned from this method, only the reduced value.
--- 11-Arrays/script.js
+++ 11-Arrays/script.js
@@ -90,8 +90,15 @@ const createUsernames = function (accs) {
   });
 };
 
-displayMovements(account1.movements);
+const calcPrintBalance = function (movements) {
+  const balance = movements.reduce((acc, curr) => acc + curr, 0);
+
+  labelBalance.textContent = `${balance} EUR`;
+};
+
 createUsernames(accounts);
+displayMovements(account1.movements);
+calcPrintBalance(account1.movements);
 /* 
 ////////////////////////
  Coding Challenge 1
@@ -149,3 +156,64 @@ const juliasDataTwo = [9, 16, 6, 8, 3];
 const katesDataTwo = [10, 5, 6, 1, 4];
 checkDogs(juliasDataTwo, katesDataTwo);
 // CHALLENGE 2 END
+
+/* 
+ ////////////////////////
+ Coding Challenge 2
+///////////////////////
+
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert 
+dog ages to human ages and calculate the average age of the dogs in their study. 
+Your tasks: 
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's 
+ages ('ages'), and does the following things in order: 
+1. Calculate the dog age in human years using the following formula: if the dog is 
+<= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, 
+humanAge = 16 + dogAge * 4 
+2. Exclude all dogs that are less than 18 human years old (which is the same as 
+keeping dogs that are at least 18 years old) 
+3. Calculate the average human age of all adult dogs (you should already know 
+from other challenges how we calculate averages 😉) 
+4. Run the function for both test datasets 
+Test data: 
+§ Data 1: [5, 2, 4, 1, 15, 8, 3] 
+§ Data 2: [16, 6, 10, 5, 6, 1, 4] 
+ 
+GOOD LUCK 😀 
+*/
+
+const calcAverageHumanAge = function (dogs) {
+  const toHumanAge = dogs.map(dog => {
+    if (dog <= 2) return dog * 2;
+    else return dog * 4 + 16;
+  });
+
+  const over18 = toHumanAge.filter(age => age >= 18);
+
+  const ageAverage = toHumanAge.reduce(
+    (acc, curr) => (acc + curr) / toHumanAge.length
+  );
+
+  return {
+    humanAges: toHumanAge,
+    over18,
+    ageAverage,
+  };
+};
+
+console.log('-----------CHALLENGE #2-----------');
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+
+/* 
+  ////////////////////////
+ Coding Challenge 3
+///////////////////////
+  Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time 
+  as an arrow function, and using chaining! 
+  Test data: 
+  § Data 1: [5, 2, 4, 1, 15, 8, 3] 
+  § Data 2: [16, 6, 10, 5, 6, 1, 4] 
+  
+  GOOD LUCK 😀 
+*/
