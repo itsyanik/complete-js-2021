56215ce14d4581f200ba26165327a52ba8bb9d3e
itsYanik <allan.yanik@gmail.com>
2022-01-28 16:29:21 -0300

    Bankist: Delete account

---
+++ 11-Arrays/07-findIndex.md
@@ -0,0 +1,4 @@
+# FindIndex
+
+This one is very similar to the `find` method, but instead of returning the element, it returns the index where that element can be found.
+`arr.findIndex((curr, index, arr) => condition)`
--- 11-Arrays/script.js
+++ 11-Arrays/script.js
@@ -166,6 +166,23 @@ btnTransfer.addEventListener('click', function (event) {
     recieverAccount.movements.push(amount);
   }
 });
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    accounts.splice(index, 1);
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = '';
+});
 /* 
 ////////////////////////
  Coding Challenge 1
