190e197a3381ec177fc229e9a1e6deb7d35a937d
itsYanik <allan.yanik@gmail.com>
2022-01-28 15:28:27 -0300

    Find method

---
+++ 11-Arrays/06-find-method.md
@@ -0,0 +1,4 @@
+# Find
+
+Just as the name suggests, we can use the find method to find an element in our array based on the condition that's given to it, which must result into a boolen value. `arr.find(el => condition)`.
+This method only returns the FIRST value in which the condition is satisfied.
--- 11-Arrays/script.js
+++ 11-Arrays/script.js
@@ -248,22 +248,3 @@ const calcAverageHumanAgeChain = dogs =>
     .reduce((acc, curr, _, arr) => acc + curr / arr.length, 0);
 console.log(calcAverageHumanAgeChain([5, 2, 4, 1, 15, 8, 3]));
 console.log(calcAverageHumanAgeChain([16, 6, 10, 5, 6, 1, 4]));
-// const calcAverageHumanAgeChain = function (dogs) {
-
-//   const toHumanAge = dogs.map(dog => {
-//     if (dog <= 2) return dog * 2;
-//     else return dog * 4 + 16;
-//   });
-
-//   const over18 = toHumanAge.filter(age => age >= 18);
-
-//   const ageAverage = toHumanAge.reduce(
-//     (acc, curr) => (acc + curr) / over18.length
-//   );
-
-//   return {
-//     humanAges: toHumanAge,
-//     over18,
-//     ageAverage,
-//   };
-// };
