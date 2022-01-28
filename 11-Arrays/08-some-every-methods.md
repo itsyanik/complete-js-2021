7549d1a02ae49834a9a42feba671d265b5e983e9
itsYanik <allan.yanik@gmail.com>
2022-01-28 16:49:24 -0300

    Every and Some methods

---
+++ 11-Arrays/08-some-every-methods.md
@@ -0,0 +1,7 @@
+# Some
+
+The some method returns true if any of the elements in the array satisfies the condition that is given to it. `arr.some((el, index, arr) => condition)`
+
+# Every
+
+In the other hand, the `every` method will only return true if ALL elements satisfy the given condition. `arr.every((el, index, arr) => condition)`.
--- 11-Arrays/script.js
+++ 11-Arrays/script.js
@@ -167,6 +167,20 @@ btnTransfer.addEventListener('click', function (event) {
   }
 });
 
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    currentAccount.movements.push(amount);
+
+    updateUI();
+  }
+
+  inputLoanAmount.value = '';
+});
+
 btnClose.addEventListener('click', function (e) {
   e.preventDefault();
 
