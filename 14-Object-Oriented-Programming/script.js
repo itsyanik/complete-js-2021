d3a09cbf21ee6e52c785d61aaba168fe6335c79f
itsYanik <allan.yanik@gmail.com>
2022-02-09 12:34:55 -0300

    Answers challenge 4

--- 14-Object-Oriented-Programming/script.js
+++ 14-Object-Oriented-Programming/script.js
@@ -81,6 +81,8 @@ class CarCl {
   brake() {
     this.speed -= 5;
     console.log(`${this.make} going at ${this.speed}`);
+
+    return this;
   }
 
   get speedUs() {
@@ -154,3 +156,47 @@ tesla.chargeBattery(90);
 tesla.accelerate();
 tesla.brake();
 console.log(tesla);
+
+// Challenge #4
+/* Your tasks: 
+1. Re-create Challenge #3, but this time using ES6 classes: create an 'EVCl' 
+child class of the 'CarCl' class 
+2. Make the 'charge' property private 
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' 
+methods of this class, and also update the 'brake' method in the 'CarCl' 
+class. Then experiment with chaining! 
+Test data: 
+§ Data car 1: 'Rivian' going at 120 km/h, with a charge of 23% 
+ 
+GOOD LUCK 😀 */
+
+class EVCl extends CarCl {
+  #charge;
+
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    this.#charge -= 1;
+
+    return this;
+  }
+
+  chargeBattery(chargeTo) {
+    if (chargeTo >= 100) this.#charge = 100;
+    this.#charge = chargeTo;
+
+    return this;
+  }
+}
+
+const rivian = new EVCl('Rivian', 120, 23);
+rivian.accelerate();
+rivian.brake();
+rivian.accelerate().chargeBattery(90);
+rivian.brake().brake();
+
+console.log(rivian);
