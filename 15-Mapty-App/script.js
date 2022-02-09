e6471f05af1362f83d67970d71a920be647cd2f3
itsYanik <allan.yanik@gmail.com>
2022-02-09 15:50:53 -0300

    Adds map from leaflet

--- 15-Mapty-App/script.js
+++ 15-Mapty-App/script.js
@@ -10,3 +10,44 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// CONSTANTS
+const MAP_ZOOM_LEVEL = 13;
+
+// Prevents old browsers from crashing
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, MAP_ZOOM_LEVEL);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on('click', function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              autoClose: false,
+              className: '.running-popup',
+              closeOnClick: false,
+              maxWidth: 250,
+              minWidth: 100,
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
+}
