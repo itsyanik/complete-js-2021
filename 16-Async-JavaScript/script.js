02c786ae949e72ea5ab28b982d87d4e364bed00c
itsYanik <allan.yanik@gmail.com>
2022-02-10 10:24:01 -0300

    Promises

--- 16-Async-JavaScript/script.js
+++ 16-Async-JavaScript/script.js
@@ -3,10 +3,10 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const restCountriesURL = 'https://restcountries.com/v2';
 ///////////////////////////////////////
 
 function getCountryData(country) {
-  const restCountriesURL = 'https://restcountries.com/v2/';
   // Old School way
   const request = new XMLHttpRequest();
   request.open('GET', `${restCountriesURL}/name/${country}`);
@@ -14,28 +14,42 @@ function getCountryData(country) {
 
   request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText);
-    const { flag, name, region, population, languages, currencies } = data;
-    const language = languages[0].name;
-    const currency = currencies[0].name;
-
-    const roundedPop = (Number(population) / 1000000).toFixed(1);
-
-    const html = `
-  <article class="country">
-    <img class="country__img" src=${flag} />
-    <div class="country__data">
-      <h3 class="country__name">${name}</h3>
-      <h4 class="country__region">${region}</h4>
-      <p class="country__row"><span>👫</span>${roundedPop}</p>
-      <p class="country__row"><span>🗣️</span>${language}</p>
-      <p class="country__row"><span>💰</span>${currency}</p>
-    </div>
-</article>`;
-
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+    renderCountry(data);
   });
+}
+
+function renderCountry(data) {
+  const { flag, name, region, population, languages, currencies } = data;
+  const language = languages[0].name;
+  const currency = currencies[0].name;
+
+  const roundedPop = (Number(population) / 1000000).toFixed(1);
+
+  const html = `
+    <article class="country">
+      <img class="country__img" src=${flag} />
+      <div class="country__data">
+        <h3 class="country__name">${name}</h3>
+        <h4 class="country__region">${region}</h4>
+        <p class="country__row"><span>👫</span>${roundedPop}</p>
+        <p class="country__row"><span>🗣️</span>${language}</p>
+        <p class="country__row"><span>💰</span>${currency}</p>
+      </div>
+    </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
 }
 
 getCountryData('Portugal');
 getCountryData('United Kingdom');
+
+// New modern way
+
+const getCountryDataPromise = function (country) {
+  fetch(`${restCountriesURL}/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+
+getCountryDataPromise('Mexico');
